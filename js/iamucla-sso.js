1d6f0b8f67b70f50cb601c4198bfc293f19dfacf
Carl Uebelhart <carl@erutan.net>
2014-11-04 17:20:52 -0800

    a11y, naming

--- js/iamucla-sso.js
+++ js/iamucla-sso.js
@@ -3,3 +3,11 @@
 $(function () {
 	$(document).placeholdr();
 });
+
+// js autofocus for older browsers
+
+$(function () {
+	if (!("autofocus" in document.createElement("input"))) {
+		document.getElementById("logon").focus();
+	}
+});

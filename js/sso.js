99c5cd997fb87c39462d0ee6036758485c6a66c8
Carl Uebelhart <carl@erutan.net>
2015-03-18 23:10:27 -0700

    better form error styling

--- js/sso.js
+++ js/sso.js
@@ -1,10 +1,14 @@
 // load & configure webshims
 
 webshim.setOptions('forms', {
-	//set lazyCustomMessages to true
-	lazyCustomMessages: true,
-	//show custom styleable validation bubble
-	replaceValidationUI: true
+  //set lazyCustomMessages to true
+  lazyCustomMessages: true,
+  //show custom styleable validation bubble
+  replaceValidationUI: true,
+  iVal: {
+    "recheckDelay": 200,
+    "errorBoxClass": "error-box"
+  }
 });
 
 //start polyfilling
@@ -15,17 +19,17 @@ webshim.polyfill('forms');
 // disable submit if inputs not filled
 
 $(document).ready(function() {
-	var $submit = $("button[type=submit]"),
-	  $inputs = $('input[type=text], input[type=password]');
-	
-	function checkEmpty() {
-		// filter over the empty inputs
-		return $inputs.filter(function() {
-		    return !$.trim(this.value);
-		}).length === 0;
-	}
-	
-	$inputs.on('keyup blur', function() {
-		$submit.prop("disabled", !checkEmpty());
-	}).blur(); // trigger an initial blur
+  var $submit = $("button[type=submit]"),
+    $inputs = $('input[type=text], input[type=password]');
+  
+  function checkEmpty() {
+    // filter over the empty inputs
+    return $inputs.filter(function() {
+        return !$.trim(this.value);
+    }).length === 0;
+  }
+  
+  $inputs.on('keyup blur', function() {
+    $submit.prop("disabled", !checkEmpty());
+  }).blur(); // trigger an initial blur
 });
